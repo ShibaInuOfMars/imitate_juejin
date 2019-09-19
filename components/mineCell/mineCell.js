33563effd4bcafe2fff7c854f9c326c54e5ae839
ShibaInuOfMars <1051858676@qq.com>
2019-09-19 17:11:02 +0800

    我的界面制作完成

---
+++ components/mineCell/mineCell.js
@@ -0,0 +1,36 @@
+// components/mineCell/mineCell.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    functionIcon: {
+      type: String,
+      value: "/assets/img/ic_login_logo.png"
+    },
+    functionText: {
+      type: String,
+      value: "掘金社区"
+    },
+    megCount: {
+      type: String,
+      value: ""
+    }
+  },
+
+  externalClasses: ["special-text"],
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
